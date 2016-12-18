88675b0a0fffff59508aa8c0512e24c531501270
yyyk <yasuyuki.ogawa@beproud.jp>
2016-12-18 19:01:40 +0900

    revert test require statement because scafold makes differ code

--- test/unit/glossary_style_test.rb
+++ test/unit/glossary_style_test.rb
@@ -1,6 +1,8 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class GlossaryStyleTest < ActiveSupport::TestCase
+  fixtures :glossary_styles
+
   # Replace this with your real tests.
   def test_truth
     assert true
--- test/unit/term_category_test.rb
+++ test/unit/term_category_test.rb
@@ -1,6 +1,8 @@
-require 'test_helper'
+require File.dirname(__FILE__) + '/../test_helper'
 
 class TermCategoryTest < ActiveSupport::TestCase
+  fixtures :term_categories
+
   # Replace this with your real tests.
   def test_truth
     assert true
